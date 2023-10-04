23a6ac5924334b6c55d3c63f7d63d52473e2d4ee
xl_19940626@sina.com <xl_19940626@sina.com>
2023-10-04 11:27:37 +0800

    FINAL PIPELINE combining STAR INDEX, SITE01 and SITE02

--- STAR_FINAL_PIPELINE_all.sh
+++ STAR_FINAL_PIPELINE_all.sh
@@ -4,16 +4,16 @@
 reads_length=xxx # need to specify accordingly
 mkdir STAR_INDEX
 cd STAR_INDEX
-STAR --runThreadN 10 --sjdbOverhang `expr ${reads_length} - 1` --runMode genomeGenerate --sjdbGTFfile /public/home/Songlab/songyl/database/annotation/human/gencode.v19.chr_patch_hapl_scaff.annotation.gtf --genomeFastaFiles /public/home/Songlab/songyl/database/genome/human/GencodeV19/STAR_index/with_GencodeV19_gtf/GRCh37.p13.genome.fa --genomeDir ./ > STAR_index.log 2>&1 &
+STAR --runThreadN 10 --sjdbOverhang `expr ${reads_length} - 1` --runMode genomeGenerate --sjdbGTFfile /public/home/Songlab/songyl/database/annotation/human/gencode.v19.chr_patch_hapl_scaff.annotation.gtf --genomeFastaFiles /public/home/Songlab/songyl/database/genome/human/GencodeV19/STAR_index/with_GencodeV19_gtf/GRCh37.p13.genome.fa --genomeDir ./ > STAR_index.log 2>&1
 cd ../
-wait
+## ATTENTION: after the script has finished, whether STAR INDEX had finished should be checked before SJM the job
 
 # Basic options
-FASTQ_DIR=xxx # need to specify accordingly
 home_wd=`pwd`
+FASTQ_DIR=${home_wd}/FASTQ/trim_reads # need to specify accordingly
 STAR_INDEX=${home_wd}/STAR_INDEX # /need to specify accordingly, e.g. /public/home/Songlab/xial/Work/cell_line_dataset/HEK293T/GSE99249/STAR_index/
 UNIQUE_ID=${home_wd##*/} # to specify if needed
-SraFile=FASTQ/SraAccList.csv # to specify if needed
+SraFile=${home_wd}/FASTQ/SraAccList.csv # to specify if needed
 SAMPLEs=($(cat ${SraFile}))
 Alu='Both' # to specify if needed {'Both', 'Alu', 'nonAlu'}
 
@@ -47,15 +47,17 @@ for SAMPLE in ${SAMPLEs[@]}; do
     touch ${SAMPLE}/starsplice_${SAMPLE}_1/${sample_name}_1_GATKRECAL.filtered.bam
     touch ${SAMPLE}/starsplice_${SAMPLE}_2/${sample_name}_2_GATKRECAL.filtered.bam
     x=`expr ${x} + 1`
-    echo -e "order ${sample_name}_1_GATKCallEdits before ${SAMPLE}_CHANGE" >> ${UNIQUE_ID}.order # ${SAMPLE_list[x]}_First
-    echo -e "order ${sample_name}_2_GATKCallEdits before ${SAMPLE}_CHANGE" >> ${UNIQUE_ID}.order
+    echo -e "order ${sample_name}_1_GATKCallEdits before ${SAMPLE}_01_CHANGE" >> ${UNIQUE_ID}.order # ${SAMPLE_list[x]}_First
+    echo -e "order ${sample_name}_2_GATKCallEdits before ${SAMPLE}_01_CHANGE" >> ${UNIQUE_ID}.order
     if [[ ${x} -lt ${#SAMPLEs[@]} ]]; then
-        echo -e "order ${SAMPLE}_CHANGE before ${SAMPLEs[x]}_1_First" >> ${UNIQUE_ID}.order
-        echo -e "order ${SAMPLE}_CHANGE before ${SAMPLEs[x]}_2_First" >> ${UNIQUE_ID}.order
+        echo -e "order ${SAMPLE}_01_CHANGE before ${SAMPLEs[x]}_1_First" >> ${UNIQUE_ID}.order
+        echo -e "order ${SAMPLE}_01_CHANGE before ${SAMPLEs[x]}_2_First" >> ${UNIQUE_ID}.order
+    else
+        echo -e "order ${SAMPLE}_01_CHANGE before site01_CHANGE" >> ${UNIQUE_ID}.order
     fi
     grep -vE 'order|log_dir' ${SAMPLE}/${sample_name}_1_star.sjm >> ${UNIQUE_ID}.cmd
     grep -vE 'order|log_dir' ${SAMPLE}/${sample_name}_2_star.sjm >> ${UNIQUE_ID}.cmd
-    echo -e "job_begin\n name ${SAMPLE}_CHANGE\n memory 3G\n directory `pwd`\n cmd_begin" >> ${UNIQUE_ID}.cmd
+    echo -e "job_begin\n name ${SAMPLE}_01_CHANGE\n memory 3G\n directory `pwd`\n cmd_begin" >> ${UNIQUE_ID}.cmd
     echo -e "/usr/bin/date;" >> ${UNIQUE_ID}.cmd
     echo -e "cmd_end\njob_end\n" >> ${UNIQUE_ID}.cmd
 done
@@ -71,21 +73,21 @@ for SAMPLE in ${SAMPLEs[@]}; do
 done
 
 x=0
+echo -e "order site01_CHANGE before ${SAMPLEs[0]}_MERGE" >> ${UNIQUE_ID}.order
 for SAMPLE in ${SAMPLEs[@]}; do
     sample_name=${SAMPLEs[x]}
-    echo -e "order site01_CHANGE before ${sample_name}_MERGE" >> ${UNIQUE_ID}.order
     grep 'order' ${SAMPLE}/${sample_name}.bam.sjm >> ${UNIQUE_ID}.order
     x=`expr ${x} + 1`
     if [[ ${Alu} == "Alu" || ${Alu} == "nonAlu" ]]; then
-        echo -e "order ${sample_name}_CAT before ${SAMPLE}_CHANGE" >> ${UNIQUE_ID}.order
+        echo -e "order ${sample_name}_CAT before ${SAMPLE}_02_CHANGE" >> ${UNIQUE_ID}.order
     else
-        echo -e "order ${sample_name}_CAT_NoAlu before ${SAMPLE}_CHANGE" >> ${UNIQUE_ID}.order
+        echo -e "order ${sample_name}_CAT_NoAlu before ${SAMPLE}_02_CHANGE" >> ${UNIQUE_ID}.order
     fi
     if [[ ${x} -lt ${#SAMPLEs[@]} ]]; then
-        echo -e "order ${SAMPLE}_CHANGE before ${SAMPLEs[x]}_MERGE" >> ${UNIQUE_ID}.order
+        echo -e "order ${SAMPLE}_02_CHANGE before ${SAMPLEs[x]}_MERGE" >> ${UNIQUE_ID}.order
     fi
     grep -vE 'order|log_dir' ${SAMPLE}/${sample_name}.bam.sjm >> ${UNIQUE_ID}.cmd
-    echo -e "job_begin\n name ${SAMPLE}_CHANGE\n memory 3G\n directory `pwd`\n cmd_begin" >> ${UNIQUE_ID}.cmd
+    echo -e "job_begin\n name ${SAMPLE}_02_CHANGE\n memory 3G\n directory `pwd`\n cmd_begin" >> ${UNIQUE_ID}.cmd
     echo -e "/usr/bin/date;" >> ${UNIQUE_ID}.cmd
     echo -e "cmd_end\njob_end\n" >> ${UNIQUE_ID}.cmd
 done
